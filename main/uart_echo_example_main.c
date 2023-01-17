3399c9fab8fada8ec75a47ebd59f8848bb5e9f29
Zahra Taki <z.taki@pardisan.local>
2023-01-17 16:55:26 +0330

    updating

--- main/uart_echo_example_main.c
+++ main/uart_echo_example_main.c
@@ -18,17 +18,34 @@ static const char *TAG = "uart";
 #define ECHO_UART_BAUD_RATE     (115200)
 #define ECHO_TASK_STACK_SIZE    (2048)
 #define TIMEOUT (20 / portTICK_RATE_MS)
+#define xDelay (1000 / portTICK_PERIOD_MS)
 #define BUF_SIZE (256)
 
-static void update_time(char[] data){
+char uart_recv_data[BUF_SIZE] = "";
+struct time
+{
+    int hh;
+    int mm;
+    int ss;
+} myTime;
+
+static void update_time()
+{
     const char temp[BUF_SIZE] = "t:";
-    if (strncmp(data, temp, 2)==0){
+    if (strncmp(uart_recv_data, temp, 2)==0){
+        //parse into myTime
+        myTime.hh = 10;
+        myTime.mm = 20;
+        myTime.ss = 30;
+
         uart_write_bytes(ECHO_UART_PORT_NUM, "time updated", 12);
     }
+    uart_recv_data[0] = '\0';
 }
 
-static void query_time(){
-    const char query[BUF_SIZE] = "query";
+static void query_time()
+{
+    static const char query[5] = "query";
     uart_write_bytes(ECHO_UART_PORT_NUM, query, 5);
 }
 
@@ -47,22 +64,20 @@ static void time_task(void *arg)
     uart_driver_install(ECHO_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags);
     uart_param_config(ECHO_UART_PORT_NUM, &uart_config);
     uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS);
-
-    char data[BUF_SIZE] = "";
     
     while (1) {
         //request time
         query_time();
 
         //read data
-        int len = uart_read_bytes(ECHO_UART_PORT_NUM, data, BUF_SIZE, TIMEOUT);
+        uart_read_bytes(ECHO_UART_PORT_NUM, uart_recv_data, BUF_SIZE, TIMEOUT);
 
         //update time
-        update_time(data)
+        update_time(uart_recv_data);
+        ESP_LOGI(TAG, "%d:%d:%d", myTime.hh, myTime.mm, myTime.ss);
 
         //delay
-        
-        uart_write_bytes(ECHO_UART_PORT_NUM, (const char *) data, len);
+        vTaskDelay(xDelay);
     }
 }
 
